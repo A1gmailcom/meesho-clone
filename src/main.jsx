4957bd558094327e8e7068aa2a9b6d3cf9a01e8c
shankar <121998455+A1gmailcom@users.noreply.github.com>
2025-05-20 22:38:06 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
-import store from "../Redux/store"; // Import your Redux store
+import store from "./Redux/store";; // Import your Redux store
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
